aab8761ba00f2d5508781fa019cd942074e1a7a5
knknkn1162 <knknkn1162@gmail.com>
2017-12-20 15:19:23 +0900

    test Todo & AddTodoButton

--- src/components/AddTodoButton.test.tsx
+++ src/components/AddTodoButton.test.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import AddTodoButton from './AddTodoButton';
+import * as sinon from 'sinon';
 
 import * as Adapter from "enzyme-adapter-react-16";
 
 enzyme.configure({ adapter: new Adapter() });
 
-it("renders input", () => {
-  let res: string;
-  const onDummy = (s: string) => {return;};
-  const wrapper = enzyme.shallow(<AddTodoButton onSubmit={onDummy}/>);
-  wrapper.setState({input: "hello"});
+it("renders button text", () => {
+  const wrapper = enzyme.shallow(<AddTodoButton onSubmit={(s:string)=>{return;}}/>);
   expect(wrapper.find("button").text()).toEqual("Add Todo");
+});
+
+it("renders hello when onChange in input", () => {
+  const onSubmit = sinon.spy();
+  const wrapper = enzyme.shallow(<AddTodoButton onSubmit={onSubmit}/>);
+  expect(wrapper.find("button").text()).toEqual("Add Todo");
+
+  wrapper.find("input").simulate('change', {currentTarget: { value: 'hello' }});
+  expect(wrapper.state().input).toEqual('hello');
+});
+
+it("onSubmit in form", () => {
+  const onSubmit = sinon.spy();
+  const wrapper = enzyme.shallow(<AddTodoButton onSubmit={onSubmit}/>);
+  wrapper.setState({input: "hello"});
+  wrapper.find("form").simulate("submit", { preventDefault() {} });
+
+  expect(onSubmit.callCount).toEqual(1);
+  expect(onSubmit.args[0]).toEqual(["hello"]);
 });
--- src/components/Todo.test.tsx
+++ src/components/Todo.test.tsx
@@ -1,29 +1,41 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import Todo from './Todo';
+import * as sinon from 'sinon';
 
 import * as Adapter from "enzyme-adapter-react-16";
 
 enzyme.configure({ adapter: new Adapter() });
 
-it("renders text when completed=true", () => {
-  const onDummy = () => {return;};
-  const hello = enzyme.shallow(<Todo completed={true} text="hello" onClick={onDummy}/>);
+describe('renders', () => {
+    it("renders text when completed=true", () => {
+      const wrapper = enzyme.shallow(<Todo completed={true} text="hello" onClick={()=>{return;}}/>);
 
-  expect(hello.find("li").text()).toEqual("hello");
-  expect(hello.find("li").props().style.textDecoration).toEqual("line-through");
+      expect(wrapper.find("li").text()).toEqual("hello");
+      expect(wrapper.find("li").props().style.textDecoration).toEqual("line-through");
+    });
+
+    it("renders text when completed=false", () => {
+      const wrapper = enzyme.shallow(
+        <Todo 
+          completed={false} 
+          text="hello" 
+          onClick={()=>{return;}}
+        />
+      );
+
+      expect(wrapper.find("li").text()).toEqual("hello");
+      expect(wrapper.find("li").props().style.textDecoration).toEqual("none"); 
+    });
 });
 
-it("renders text when completed=false", () => {
-  const onDummy = () => {return;};
-  const hello = enzyme.shallow(
-    <Todo 
-      completed={false} 
-      text="hello" 
-      onClick={onDummy}
-    />
-  );
+describe('event', () => {
+  it("test onClick", () => {
+    const onClick = sinon.spy();
+    const wrapper = enzyme.shallow(<Todo completed={true} text="hello" onClick={onClick}/>);
 
-  expect(hello.find("li").text()).toEqual("hello");
-  expect(hello.find("li").props().style.textDecoration).toEqual("none"); 
+    wrapper.find("li").simulate("click");
+
+    expect(onClick.callCount).toEqual(1);
+  });
 });
