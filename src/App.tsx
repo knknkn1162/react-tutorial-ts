47559e0ec4c2fac0158824617766b685e275b429
knknkn1162 <knknkn1162@gmail.com>
2017-12-22 15:30:14 +0900

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import TodoList from "./containers/TodoList";
 import AddTodoButton from "./containers/AddTodoButton";
+import Footer from './containers/Footer';
+
 
 function app(): JSX.Element {
   return (
     <div>
       <AddTodoButton />
       <TodoList />
+      <Footer />
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider, Store } from 'react-redux'
 import { createStore } from 'redux';
-import todos from './reducers/todos';
-import { Todos } from './states/TodoState'
+import todoApp from './reducers/';
+import { TodoList } from './states/Filter'
 
 import App from './App';
 
-let store: Store<Todos> = createStore(todos, []);
+let store: Store<TodoList> = createStore(todoApp);
 
 // @@redux/INIT
 ReactDOM.render(
